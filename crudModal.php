d99a55947b1f3554701e2b42229cc9e44143d577
Fangting Ma <fm128@duke.edu>
2023-07-23 18:56:21 -0400

    reorganize

--- crudModal.php
+++ crudModal.php
@@ -1,3 +1,57 @@
+<!-- Add Product details using Modal -->
+<div class="modal fade" id="addModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+     aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Add a new product</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form method="post" action="addProduct.php">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Product Name</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Sale Price</label>
+                        <input type="number" class="form-control" id="price" min="0.01" step=".01" name="price"
+                               required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="quantity" class="form-label">Product Quantity</label>
+                        <input type="number" class="form-control" id="quantity" min="1" max="10000" name="quantity"
+                               required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="category" class="form-label">Product Category</label>
+                        <select class="form-select" id="category" aria-label="Default select example"
+                                name="category" required>
+                            <option value="" disabled selected>Select Product Category</option>
+                            <option value="Food">Food</option>
+                            <option value="Clothes">Clothes</option>
+                            <option value="Groceries">Groceries</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Product Description</label>
+                        <div class="input-group" id="description">
+                                <textarea class="form-control" aria-label="With textarea" name="description"
+                                          required></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <input type="hidden" name="csrfToken" value="<?= $csrfToken ?>">
+                    <button type="submit" class="btn btn-primary" name="addProduct" value="Add Product">Add Product</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
 <!-- Edit product details using Modal -->
 <div class="modal fade" id="edit_<?php echo $row['id']; ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
      aria-labelledby="staticBackdropLabel" aria-hidden="true">
